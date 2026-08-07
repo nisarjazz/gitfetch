3378e9869eb31a60d21e06e680ca8a01670b47e1
shankysai <61941953+shankysai@users.noreply.github.com>
2026-08-07 12:04:09 +0530

    Change echo message from 'fetch' to 'pull'

--- mailtest.sh
+++ mailtest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Checking Mail Server #2"
-echo "checking for fetch"
+echo "checking for pull"
 echo "QUIT" | nc -w 5 smtp.gmail.com 465 > /dev/null 2>&1
 if [ $? == 0 ]; then
   echo "mail server #1 is UP"
