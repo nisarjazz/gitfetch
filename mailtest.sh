fff43607ba396f6407cdab5dd24f48cf3c71b31b
shankysai <61941953+shankysai@users.noreply.github.com>
2026-08-07 11:58:58 +0530

    Update mailtest.sh

--- mailtest.sh
+++ mailtest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Checking Mail Server #1"
+echo "Checking Mail Server #2"
+echo "checking for fetch"
 echo "QUIT" | nc -w 5 smtp.gmail.com 465 > /dev/null 2>&1
 if [ $? == 0 ]; then
   echo "mail server #1 is UP"
