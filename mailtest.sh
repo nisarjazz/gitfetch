a182a0d715db366c3fe0a8f0f5d4411ce97bd348
EC2 Default User <ec2-user@ip-172-31-12-166.ap-south-1.compute.internal>
2026-08-10 05:52:28 +0000

    ssh testing commit

--- mailtest.sh
+++ mailtest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Checking Mail Server #2"
-echo "checking for pull"
+echo "checking for ssh push"
 echo "QUIT" | nc -w 5 smtp.gmail.com 465 > /dev/null 2>&1
 if [ $? == 0 ]; then
   echo "mail server #1 is UP"
